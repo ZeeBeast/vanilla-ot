da268e331a33610fa7bd7acee793215e773d64eb
Shaun Shearer <theinfernalbeasts@aol.com>
2014-11-07 08:26:39 -0500

    Update 031-vocations.lua
    Added new vocations here.

--- data/lib/031-vocations.lua
+++ data/lib/031-vocations.lua
@@ -1,17 +1,25 @@
 function isSorcerer(cid)
-	return isInArray({1, 5}, getPlayerVocation(cid))
+	return isInArray({1, 7, 13, 19}, getPlayerVocation(cid))
 end
 
 function isDruid(cid)
-	return isInArray({2, 6}, getPlayerVocation(cid))
+	return isInArray({2, 8, 14, 20}, getPlayerVocation(cid))
 end
 
 function isPaladin(cid)
-	return isInArray({3, 7}, getPlayerVocation(cid))
+	return isInArray({3, 9, 15, 21}, getPlayerVocation(cid))
 end
 
 function isKnight(cid)
-	return isInArray({4, 8}, getPlayerVocation(cid))
+	return isInArray({4, 10, 16, 22}, getPlayerVocation(cid))
+end
+
+function isNinja(cid)
+	return isInArray({5, 11, 17, 23}, getPlayerVocation(cid))
+end
+
+function isFighter(cid)
+	return isInArray({6, 12, 18, 24}, getPlayerVocation(cid))
 end
 
 function isRookie(cid)
