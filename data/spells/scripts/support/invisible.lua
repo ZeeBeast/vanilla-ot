1fd4c2e54a0d7609e226cb80c0a97f901842dafb
Shaun Shearer <theinfernalbeasts@aol.com>
2014-11-07 07:48:02 -0500

    Update invisible.lua
    Changed invis time to 15 seconds

--- data/spells/scripts/support/invisible.lua
+++ data/spells/scripts/support/invisible.lua
@@ -3,7 +3,7 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
 
 local condition = createConditionObject(CONDITION_INVISIBLE)
-setConditionParam(condition, CONDITION_PARAM_TICKS, 200000)
+setConditionParam(condition, CONDITION_PARAM_TICKS, 15000)
 setCombatCondition(combat, condition)
 
 function onCastSpell(cid, var)
